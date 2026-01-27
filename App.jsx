b091ef4c5c1b0f26848241f6816ae90eed91d73c
Dhanishashree <24bcs113@kgcas.com>
2026-01-27 12:45:46 +0530

    program 5

--- App.jsx
+++ App.jsx
@@ -1,32 +1,35 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
 function App() {
-  const [color, setColor] = useState("white");
+     const [greetings,setGreetings]=useState("")
+     useEffect(()=>{
+      let hours=new Date().getHours().toString().padStart(2,'0')
+      let mins=new Date().getMinutes().toString().padStart(2,'0')
+     let seconds=new Date().getSeconds().toString().padStart(2,'0')
+      
 
+      if(hours<12){
+        setGreetings(`Good Morning  ${hours}:${mins}:${seconds}`)
+      }
+      else if(hours>=12 && hours<=17){
+        setGreetings(`Good Afternoon  ${hours}:${mins}:${seconds}`)
+      }
+      else if(hours>=17 && hours<=19){
+        setGreetings(`Good evening  ${hours}:${mins}:${seconds}`)
+      }
+      else {
+        setGreetings(`Good Night  ${hours}:${mins}:${seconds}`)
+      }
+    })
   return (
-    <div className="container">
-      <h1 className="title">Color picker</h1>
-
-      <div className="btn-group">
-        <button className="btn red" onClick={() => setColor("red")}>
-          Red
-        </button>
-
-        <button className="btn blue" onClick={() => setColor("blue")}>
-          Blue
-        </button>
-
-        <button className="btn green" onClick={() => setColor("green")}>
-          Green
-        </button>
+   
+      <div>
+       <h1>{greetings}</h1>
       </div>
+  ) 
+  }
 
-      <div
-        className="color-box"
-        style={{ backgroundColor: color }}
-      ></div>
-    </div>
-  );
-}
-
-export default App;
+export default App
