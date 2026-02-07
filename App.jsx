8873e3ed8dd3b4e8cc2157541ba3a96893b893d9
Dhanishashree <24bcs113@kgcas.com>
2026-02-07 12:20:31 +0530

    program 8

--- App.jsx
+++ App.jsx
@@ -1,27 +1,63 @@
-import React,{useEffect,useState} from 'react'
+import React, { useState } from "react"
 
-function App() {
-  const[user,setUser]=useState([]);
-  useEffect(()=>{
-    fetch("https://jsonplaceholder.typicode.com/users")
-    .then((response)=>response.json())
-    .then((data)=>setUser(data))
-  },[])
-  return (
-    <div className='container'>
-       <h2 className='title'>UserDetails</h2>
-      <div className='user-list'> 
-        {user.map((Details)=>(
-           <div key={Details.id}>
-             <p>Name:{Details.name}</p>
-             <p>User Email:{Details.email}</p>
-            </div>
-          ))}
-        
+function Tab(){
+  const [activeTab,setactiveTab]=useState('Home')
+  return(
+    <div style={Style.Container}>
+      <h1>Tab Component</h1>
+      <div style={Style.tabContainer}>
+        <button style={activeTab === "Home" ? Style.activeBtn:Style.btn}></button>
+         <button onClick = {()=>setactiveTab('Home')}>Home</button>
+         <button style={activeTab === "About" ? Style.activeBtn:Style.btn}></button>
+         <button onClick = {()=>setactiveTab('About')}>About</button>
+         <button style={activeTab === "Contact" ? Style.activeBtn:Style.btn}></button>
+         <button onClick = {()=>setactiveTab('Contact')}>Contact</button>
+
       </div>
+      <div style={Style.contentBox}>
+         {activeTab === "Home" && (<p>This is home page</p>)}
+         
+         {activeTab === "About" && (<p>This is About us page</p>)}
 
+         {activeTab === "Contact" && (<p>This is Contact us page</p>)}
+
+      </div>
     </div>
   )
 }
-
-export default App
+const Style={
+  Container:{
+     minHeight:"100vh",
+     padding:"20px",
+     background:"lightgrey"
+  },
+  tabContainer:{
+    marginBottom:"20px"
+  },
+  btn:{
+    padding:"10 px",
+    margin:"5px",
+    cursor:"pointer",
+    background:"#ddd",
+    border:"1px solid #aaa",
+    borderRadius:"5px"
+  },
+  activeBtn:{
+    padding:"10px",
+    margin:"5px",
+    cursor:"pointer",
+    background:"#4caf50",
+    color:"white",
+    border:"1px solid #3a8e4",
+    borderRadius:"5px"
+  },
+  contentBox:{
+    padding:"20px",
+    border:"1px solid #ccc",
+    width:"300px",
+    margin:"0 auto",
+    borderRadius:"5px",
+    background:"#white"
+  },
+};
+export default Tab
